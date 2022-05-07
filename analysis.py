1b0a60cca65006a9e2f782cf85f7d084160c05c1
Oskar Ljung <oskarljung93@gmail.com>
2022-05-07 16:24:12 +0200

    Jag jobbar på en grej. correlations.py. Det är ett program som presenterar korrelationer mellan ordföljder numeriskt. heat-mappen och den hirarkiska klustringen är bra, men den kan bara beskrivas med en bild correlations.py kommer representera i princip samma data med siffror. Se kommentarer i koden

--- analysis.py
+++ analysis.py
@@ -1,9 +1,14 @@
 # Script for extracting word order distributions from CONLLU files
-
+import os 
 from tabulate import tabulate
 import sys
 
 def main():
+    #bygger om word-orders.txt varje gång programmet körs. 
+    #kanske onödigt, men då kommer åtmistone correlation.py alltid hänga med på förändringar
+    if os.path.exists("word-orders.txt"):
+        os.remove("word-orders.txt")
+    wo = open ("word-orders.txt", "w" )
     if len(sys.argv) == 1:
         print('Add an argument for the corpus collection file')
         return
@@ -11,17 +16,22 @@ def main():
     with open(sys.argv[1]) as lang_paths:
         for lang_path in lang_paths:
             language, path = lang_path.split()
-            table.append([language] + analyse_corpus(language, path))
+            #Nu är return-värdet för analyse_corpus aningen lägre. så det krävs lite meck med det
+            analysis = analyse_corpus(language, path) 
+            table.append([language] + analysis[:2])
+            wo.write(str(language) + ': ' + str(analysis[-1]) +'\n')
+
     table.sort(key = lambda x: x[2], reverse = True)
     print(tabulate(table,
         headers = ['Language', 'Sentences', 'SV/S', 'OV/O'],
         floatfmt = '.4f'
     ))
-
+    wo.close()
 def analyse_corpus(language, path):
-    print('Analysing', language)
+    print('Analysing', language) 
+     
     word_orders = {}
-    with open(path) as f:
+    with open(path, encoding='utf8') as f:
         sentence = []
         for line in f:
             if line.strip() != '':
@@ -37,6 +47,7 @@ def analyse_corpus(language, path):
     word_orders = [(word_orders[o], o) for o in word_orders]
     word_orders.sort(reverse = True)
 
+
     def proportion(l_feature, l_sample):
         feature = sum(n for (n, o) in filter(l_feature, word_orders))
         sample = sum(n for (n, o) in filter(l_sample, word_orders))
@@ -45,8 +56,9 @@ def analyse_corpus(language, path):
     return [
         sum(n for (n, o) in word_orders),
         proportion(lambda x: 'S' in x[1].split('V')[0], lambda x: 'S' in x[1]),
-        proportion(lambda x: 'O' in x[1].split('V')[0], lambda x: 'O' in x[1])
-    ]
+        proportion(lambda x: 'O' in x[1].split('V')[0], lambda x: 'O' in x[1]),
+        word_orders #vill ha med word_orders, det är vad jag vill ha in word-orders.txt
+        ]
 
 def parse(sentence):
     text = list(filter(lambda t: t[0] == '#' and 'text' in t, sentence))
@@ -80,10 +92,10 @@ def orders_in_sentence(data):
                     l.append((id, 'S'))
                 if data[id]['deprel'] == 'obj':
                     l.append((id, 'O'))
-                if data[id]['deprel'] == 'iobj':
-                    l.append((id, 'I'))
-                if data[id]['deprel'] == 'aux':
-                    l.append((id, 'X'))
+                # if data[id]['deprel'] == 'iobj': ## jag vill ignorera dessa
+                #     l.append((id, 'I'))
+                # if data[id]['deprel'] == 'aux':
+                #     l.append((id, 'X'))
         orders.append(''.join([letter for (id, letter) in sorted(l)]))
     return orders
 
---
+++ correlation.py
@@ -0,0 +1,3 @@
+# Takes output from analysis.py and, for each language, calculates a correlation for each word order and every other word order.
+# It presents the correlations numerically, and writes them to a file.
+
