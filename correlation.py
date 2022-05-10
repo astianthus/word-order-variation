a2640d270d0979461e9a3f903e0637387ed3d217
Oskar Ljung <oskarljung93@gmail.com>
2022-05-10 17:23:48 +0200

    Et voíla. correllations.py är färdig blir lite gnällig om du inte skickar in korekta ordordningar, men annars går det bra.

--- correlation.py
+++ correlation.py
@@ -1,18 +1,36 @@
-# Takes output from analysis.py and, for each language, calculates a correlation for each word order and every other word order.
-# It presents the correlations numerically, and writes them to a file.
+# takes distributions.txt and searches through it for two word orders given with a command line argument
+# serches for these word orders across all the languages in distributions.txt and prints out how much they correlate.
+# throws a bunch of warnings at you if the at least one of the command line arguments can not be found anywhere
+# Does not have a return value, so good luck imorting it anywhere
+
 import ast
-def correlate(orders):
-    for keys in orders.keys():
-        
-def main ():
-    with open("distributions.txt", 'r', encoding = "utf8") as f:
+import sys
+import numpy as np           
+
+def main (command_line):
+    # the frst word order and second word order 
+    first = (str(command_line[1])).upper()      
+    second = (str(command_line[2])).upper() 
+    # list of all the values of the first word order and second word order
+    x = []                          
+    y = []    
+
+    with open("distributions.txt", 'r', encoding = "utf8") as f:  
         lines = f.readlines()
         for line in lines:
             language = line.split()[0]
             orders = (line[line.find('{'):])
             orders = ast.literal_eval(orders)
-            correlate(orders)           
+            keys = list(orders.keys())
+            
+            if first in keys and second in keys:
+                x.append(orders[first]) 
+                y.append(orders[second])
 
+        
+    correlation = (np.corrcoef(x,y))[0,1]
+    print (first, "correlates with", second, "with a Pearsons coeficient of: \n", correlation)
+   
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
