708e49392b12152bef149429489db5857e44ced4
Oskar Ljung <oskarljung93@gmail.com>
2022-05-10 17:53:43 +0200

    strul

--- correlation-coeficient.py
+++ correlation-coeficient.py
@@ -35,33 +35,3 @@ def main (command_line):
 
 if __name__ == '__main__':
     main(sys.argv)
-=======
-import ast
-import sys
-import random
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-def read_dists():
-    result = []
-    with open('distributions.txt') as lang_dists:
-        for lang_dist in lang_dists:
-            lang, dist_str = lang_dist.split('{')
-            dist = ast.literal_eval('{' + dist_str)
-            result.append((lang, dist))
-    return result
-
-def main():
-    o1, o2 = sys.argv[1:]
-    data = read_dists()
-    x, y = [], []
-    for lang, dist in data:
-        x.append(dist[o1] if o1 in dist else 0)
-        y.append(dist[o2] if o2 in dist else 0)
-    plt.scatter(x, y)
-    plt.show()
-
-if __name__ == '__main__':
-    main()
->>>>>>> 2b5d7d064099e44f66cadae7af70ff2421eff592
